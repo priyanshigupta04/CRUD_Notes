577ec91a3b39b3663ed1873bb2c3f564b7aa564c
priyanshigupta04 <143316893+priyanshigupta04@users.noreply.github.com>
2025-09-04 01:03:41 +0530

    Backend URL is added

--- client/src/api/notesApi.js
+++ client/src/api/notesApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Make sure this matches your backend URL
+  baseURL: 'https://crud-notes-backend-6uf2.onrender.com', 
 });
 
 api.interceptors.request.use(
